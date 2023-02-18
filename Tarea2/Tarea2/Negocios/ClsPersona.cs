92f4043b3d15427da118dc3b1ece64821127919f
Jhonatan Henriquez <hg0533042022@unab.edu.sv>
2023-02-18 11:33:39 -0600

    Subiendo el proyecto

---
+++ Tarea2/Tarea2/Negocios/ClsPersona.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tarea2.Negocios
+{
+    public class ClsPersona
+    {
+        public int contarPalabra(String palabra)
+        {
+            return palabra.Length;
+        }
+    }
+}
